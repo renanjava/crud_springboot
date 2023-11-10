8c1c398d1d1e73675d84e386ba6b16ab0a42cb86
renan <renan@RenanJava>
2023-11-10 15:39:22 -0300

    ADD - Página de criar novo usuário

--- crud/src/main/java/br/edu/unicesumar/crud/controller/AutorController.java
+++ crud/src/main/java/br/edu/unicesumar/crud/controller/AutorController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.edu.unicesumar.crud.exceptions.PessoaNaoEncontradaException;
 import br.edu.unicesumar.crud.model.domain.ModelAutor;
 import br.edu.unicesumar.crud.model.repository.AutorRepository;
 
@@ -21,8 +24,29 @@ public class AutorController {
 	@GetMapping("/all")
 	public ModelAndView getAll() {
 		List<ModelAutor> autores = autorRepository.findAll();
-		ModelAndView view = new ModelAndView("autor");
+		ModelAndView view = new ModelAndView("list-autor");
 		view.addObject("autores", autores);
 		return view;
+	}
+	
+	@GetMapping("/{id}")
+	public ModelAndView getById(@PathVariable Long id) throws PessoaNaoEncontradaException {
+		//ModelAutor autorBuscado = autorRepository.findById(id).orElseGet(() -> return new ModelAndView("erro"));
+		ModelAndView view = new ModelAndView("list-autor");
+		//view.addObject("autores", autorBuscado);
+		return view;
+	}
+	
+	@GetMapping("/novo")
+	public ModelAndView newAutor() {
+		ModelAndView view = new ModelAndView("form-autor");
+		view.addObject("autor", new ModelAutor());
+		return view;
+	}
+	
+	@PostMapping
+	public String create(ModelAutor novoAutor) {
+		autorRepository.save(novoAutor);
+		return "redirect:/autor/all";
 	}
 }
